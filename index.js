8a75a0d9b88bfc4dfc4475d3b933f42d9e604ce0
Jose de Castro <jose@voxeolabs.com>
2014-06-07 12:54:22 +0200

    Shit works

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
 var tropo = require('tropo');
+var util = require('util');
 var active_calls = [];
-var limit = 3;
+var limit = 1;
+var timeout = 5000;
 
 tropo.on('subscribe', function (event) {
 
     var subscriber = event.subscriber;
     subscriber.on('call:incoming', function (event) {
     	
-    	console.log('Received incoming call');
+      console.log('Received incoming call');
+
+      var call = event.call;
+      var number = call.to.substr(0,call.to.indexOf('@'));
+      var connectAddress = 'sip:' + number + '@192.168.50.100:5080';
 
       active_calls.push(event.call);
 
-      if (active_calls.length >= limit) {
-        console.log('Disconnecting longest-running call');
-        longest_call = active_calls.shift();
+      call.connections[0].on('disconnected', function() {
+        console.log('Call is hung up, removing from list of active calls');
+        var index = active_calls.indexOf(call);
+        active_calls.splice(index, 1);
+      });
+
+      if (active_calls.length > limit) {
+        console.log('Length = ' + active_calls.length);
+        console.log('Disconnecting longest-running call ' + util.inspect(active_calls));
+        longest_call = active_calls[0];
         // TODO Find the caller's peer and copy this notification
-        longest_call.say("This call will now be disconnected.");
-        longest_call.disconnect();
+        longest_call.say("This call will now be disconnected.").on('end', function() {
+          longest_call.disconnect();
+          call.connect(connectAddress);
+        });
+        call.autoConnect = false;
       }
+      else {
+          call.connect(connectAddress);
+      }
+
     });
     
     subscriber.on('call:connected', function(event) {
         var call = event.call;
         setTimeout(function() {
     	    console.log('Notifying caller that call has exceeded guaranteed length');
-          // Minimum guaranteed time is 30 seconds
           call.say("Due to network usage, this call may be disconnected automatically.")
-        }, 30000);
+        }, timeout);
     });
 
-    subscriber.on('call:disconnected', function(event) {
-      console.log('Call is hung up, removing from list of active calls');
-      var call = event.call;
-      var index = active_calls.indexOf(call);
-      active_calls.splice(index, 1);
-    });
 });
